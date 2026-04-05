466a0e08aba21391022ae41d3fed70925eada570
Yousef Hany <116505809+yousefHanyy@users.noreply.github.com>
2026-04-05 21:27:55 +0200

    - Learned about callback and event loop and quick diagram on callback phases. -Learned how to create a basic http server.

--- session3_codeZone/index.js
+++ session3_codeZone/index.js
@@ -1,37 +1,53 @@
-const crypto = require("node:crypto");
+// const crypto = require("node:crypto");
+const http = require("node:http");
 
-process.env.UV_THREADPOOL_SIZE = 5; // Increasing the threadpool to be 5 (remember the max is 1024 although you should treat the threads as your cores so it has to make sense)
+// process.env.UV_THREADPOOL_SIZE = 5; // Increasing the threadpool to be 5 (remember the max is 1024 although you should treat the threads as your cores so it has to make sense)
 
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 ms", performance.now());
-});
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 ms", performance.now());
-});
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 ms", performance.now());
-});
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 ms", performance.now());
-});
-// The default size of the thread pool is 4, so only 4 of these will run at the same time. The others will wait until one of the threads is free.
-crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
-  console.log("End of PBKDF2 ms", performance.now());
-});
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 ms", performance.now());
+// });
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 ms", performance.now());
+// });
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 ms", performance.now());
+// });
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 ms", performance.now());
+// });
+// // The default size of the thread pool is 4, so only 4 of these will run at the same time. The others will wait until one of the threads is free.
+// crypto.pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("End of PBKDF2 ms", performance.now());
+// });
 //----------------------------------------------------------
-fetch("https://dummyjson.com/products").then(() => {
-  console.log("End of request ms", performance.now());
-});
-fetch("https://dummyjson.com/products").then(() => {
-  console.log("End of request ms", performance.now());
-});
-fetch("https://dummyjson.com/products").then(() => {
-  console.log("End of request ms", performance.now());
-});
-fetch("https://dummyjson.com/products").then(() => {
-  console.log("End of request ms", performance.now());
-});
-fetch("https://dummyjson.com/products").then(() => {
-  console.log("End of request ms", performance.now());
-});
+// fetch("https://dummyjson.com/products").then(() => {
+//   console.log("End of request ms", performance.now());
+// });
+// fetch("https://dummyjson.com/products").then(() => {
+//   console.log("End of request ms", performance.now());
+// });
+// fetch("https://dummyjson.com/products").then(() => {
+//   console.log("End of request ms", performance.now());
+// });
+// fetch("https://dummyjson.com/products").then(() => {
+//   console.log("End of request ms", performance.now());
+// });
+// fetch("https://dummyjson.com/products").then(() => {
+//   console.log("End of request ms", performance.now());
+// });
 // Although these are also more than 4, they seem to have the same execution time. That is because this isn't managed by the thread pool but rather managed by the os kernels (differs from each os).This is where node.js shines, it shines in network based processes more than cpu related tasks.
+
+const server = http.createServer((req, res) => {
+  console.log("Request", req.url);
+  if (req.url === "/") {
+    res.end("Home page!");
+  } else if (req.url === "/products") {
+    res.end("Products page!");
+  } else {
+    res.end("Page not found!");
+  }
+});
+
+server.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
